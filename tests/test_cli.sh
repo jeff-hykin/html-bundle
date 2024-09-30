7ba4b4cf1ce4a92afa2be94fa1377199549fd552
Jeff Hykin <jeff.hykin@gmail.com>
2024-09-30 12:22:08 -0500

    add cli tests

---
+++ tests/test_cli.sh
@@ -0,0 +1,2 @@
+deno run -A ../main/html-bundle.js --shouldBundleCss false ../test_content/test1/index.html ../logs/cli_test1/index.no_css.html
+deno run -A ../main/html-bundle.js --shouldBundleScripts false ../test_content/test1/index.html ../logs/cli_test1/index.no_scripts.html

afabec2955aec5d4cebf506b8e64e92f4c26bedf
Jeff Hykin <jeff.hykin@gmail.com>
2025-04-29 09:40:32 -0500

    enhance tools

--- main/tools.js
+++ main/tools.js
@@ -1,3 +1,4 @@
+export { default as escapeHtml } from "https://esm.sh/lodash@4.17.21/es2022/escape.mjs"
 export function escapeForStyleTag(string) {
     return string.replace(/<\/style>/g, "\\003C/style>")
 }
